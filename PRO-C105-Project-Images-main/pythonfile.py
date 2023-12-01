b4d873412ed38a8cf46c827a66d888198b2245f6
Adriana1234567 <111986480+Adriana1234567@users.noreply.github.com>
2023-12-01 18:08:24 +0000

    Add files via upload

---
+++ PRO-C105-Project-Images-main/pythonfile.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,channels = frame.shape
+size= (width,height)
+
+video = cv2.VideoWriter("video.mp4",cv2.VideoWriter_fourcc(*"DIVX"), 0.8 , size)
+for j in range(count-1, 0 , -1):
+    frame = cv2.imread(images[j])
+    video.write(frame)
+video.release()
